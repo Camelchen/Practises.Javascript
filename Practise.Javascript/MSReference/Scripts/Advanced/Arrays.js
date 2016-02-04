128de2d175cbd09d6146009d3ec79367f53041c7
zhuoruic <zhuoruic@worldpay.ca>
2016-02-04 17:43:13 -0500

    2016-02-04, Arrays

--- Practise.Javascript/MSReference/Scripts/Advanced/Arrays.js
+++ Practise.Javascript/MSReference/Scripts/Advanced/Arrays.js
@@ -5,6 +5,19 @@ var entryNum = 5;
 sample = new Array();
 
 sample[1] = "Maple Street";
-sample[entryNum] = "test";
+sample[entryNum] = "25";
 
 console.log(sample);
+ShowOutput(sample[1]);
+ShowOutput(sample[entryNum]);
+
+//property array
+
+ShowOutput("//property array");
+sample["a"] = "test1";
+sample["b"] = "test3";
+sample["c"] = "test4";
+
+for (var s in sample) {
+    ShowOutput(s + " : " + sample[s]);
+}
