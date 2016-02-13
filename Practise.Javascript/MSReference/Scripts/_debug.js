d6e15bd509dcc410202075001a32c18831dd2142
zhuoruic <zhuoruic@worldpay.ca>
2016-02-13 11:55:44 -0500

    add viewDebug

--- Practise.Javascript/MSReference/Scripts/_debug.js
+++ Practise.Javascript/MSReference/Scripts/_debug.js
@@ -3,44 +3,31 @@ ShowHeader(0);
 
 
 //json
-ShowOutput("this is debug info.");
+ShowOutput("Here is debug info in script.");
 
 var text = '{"name":"John","street":"montreal","phone":"514 1234567"}';
 
 var obj = JSON.parse(text);
 console.dir(obj);
 
-document.getElementById('demo').innerHTML = obj.name + obj.street + obj.phone;
-
+document.getElementById("demo").innerHTML += "&nbsp &nbsp &nbsp" + obj.name + ", " + obj.street + ", " + obj.phone;
 
 //bind
 var data;
-
-var dataReadyEvent = document.createEvent("Event");
-dataReadyEvent.initEvent("dataReady", true, false);
 
 function DataObject() {
     this.name = "Data Object";
     this.data = function () {
         return data;
     }
-
     this.onDataCompleted = dataReadyHandler;
-    document.addEventListener("dataReady", this.onDataCompleted.bind(this));
-
 }
 
 function dataReadyHandler() {
     if (console && console.log) {
-        console.log("Data object property value(name): " + this.name);
-        console.log("Data object property value(data): " + this.data());
+        console.log("Data object property value(name): ", this.name);
+        console.log("Data object property value(data): ", this.data());
     }
 }
 
-setTimeout(function() {
-    data = [0, 1, 2, 3];
-    document.dispatchEvent(dataReadyEvent);
-}, 5000);
-
-
-var dataObj = new DataObject();
+var data = new DataObject();
